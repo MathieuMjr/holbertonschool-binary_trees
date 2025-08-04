20e61a4765946425d7bf93d8e4182e3d3867632b
Mathieu <mathieu.mjr@gmail.com>
2025-08-04 14:38:37 +0200

    fixed parent null case + coding style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,14 +13,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 	binary_tree_t *next_left;
 
+	if (parent == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL || parent == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->right = NULL;
-	
+
 
 	if (parent->left != NULL)
 	{
@@ -34,6 +36,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left = NULL;
 		parent->left = new_node;
 	}
-		
+
 	return (new_node);
 }
