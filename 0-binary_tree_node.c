98610af1ded8cf61059c23c0bd431570114c10b0
Mathieu <mathieu.mjr@gmail.com>
2025-08-04 12:28:48 +0200

    fixed coding style and description

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - create a new node
+ * @parent: adress of the parent node
+ * @n: value of the node
+ *
+ * Return: adresse of the new node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
@@ -12,5 +19,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	return(new_node);
+	return (new_node);
 }
