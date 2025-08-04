63a22cd4ca01acee70257da26fa8022db0ad785c
Mathieu <mathieu.mjr@gmail.com>
2025-08-04 14:36:50 +0200

    fixed parent ->left assignation in case parent_left is null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,6 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->right = NULL;
+	
 
 	if (parent->left != NULL)
 	{
@@ -29,7 +30,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = new_node;
 	}
 	else
+	{
 		new_node->left = NULL;
+		parent->left = new_node;
+	}
 		
 	return (new_node);
 }
