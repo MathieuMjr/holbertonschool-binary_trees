9442fca8a15bfc3c02d4b23318bd648911f2e803
Mathieu <mathieu.mjr@gmail.com>
2025-08-06 11:51:23 +0200

    task 16 files

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - tells if a tree
+ * is perfect or not (all leaves are at
+ * the same level. All internal nodes
+ * have 2 children)
+ * @tree: adresse of the root of a tree
+ *
+ * Return: 1 if tree is perfect, 0 else
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree = NULL)
+		return (0);
+
+	
+}
